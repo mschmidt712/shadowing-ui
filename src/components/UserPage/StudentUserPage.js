6c5eee0bcc90c5237e6f1ddb1b3b83aa162fc90e
mschmidt712 <mschmidt@kenzan.com>
2019-04-30 13:12:00 -0600

    Add formatting to student user page to match doctor user page.

--- src/components/UserPage/StudentUserPage.js
+++ src/components/UserPage/StudentUserPage.js
@@ -16,18 +16,6 @@ export default function StudentUserPage(props) {
     <div>
       <h1 className="user-header"><i className="fa fa-user icon"></i>{props.name}</h1>
       <div className="user-body">
-        <div className="user-data">
-          <h3 className="app-subtitle">Email: {props.email}</h3>
-          <h3 className="app-subtitle">Phone Number: {props.phoneNumber}</h3>
-          <h3 className="app-subtitle">Address: {`${props.address.streetAddress}, ${props.address.city}, ${props.address.state} ${props.address.zipCode}`}</h3>
-          <h3 className="app-subtitle">
-            HIPAA Certified
-            <i className={`fa ${hipaaIcon} icon`}></i>
-          </h3>
-          <Link to="/sign-up/student">
-            <button className="secondary">Edit Profile</button>
-          </Link>
-        </div>
         <div className="user-map">
           <UserPageMapComponent
             googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyBV0ERwNWnf4cLICe7TozgRJG6jNM5aL9Q"
@@ -35,6 +23,27 @@ export default function StudentUserPage(props) {
             containerElement={<div style={{ height: `400px` }} />}
             mapElement={<div style={{ height: `100%` }} />}
             addressLatLong={props.addressLatLong} />
+        </div>
+        <div className="user-data">
+          <div className="data-item">
+            <h3 className="app-subtitle">Email:</h3>
+            {props.email}
+          </div>
+          <div className="data-item">
+            <h3 className="app-subtitle">Phone Number:</h3>
+            {props.phoneNumber}
+          </div>
+          <div className="data-item">
+            <h3 className="app-subtitle">Address:</h3>
+            {props.address.streetAddress} {props.address.city}, {props.address.state} {props.address.zipCode}
+          </div>
+          <div className="data-item">
+            <h3 className="app-subtitle">HIPAA Certified</h3>
+            <i className={`fa ${hipaaIcon} icon`}></i>
+          </div>
+          <Link to="/sign-up/student">
+            <button className="secondary">Edit Profile</button>
+          </Link>
         </div>
       </div>
     </div>
