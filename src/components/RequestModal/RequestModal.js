4f6e94d57fb7153dc3eff2330a22612868f15dcb
mschmidt712 <mschmidt@kenzan.com>
2020-03-10 09:17:06 -0600

    Remove provider name from request modal.

--- src/components/RequestModal/RequestModal.js
+++ src/components/RequestModal/RequestModal.js
@@ -141,7 +141,7 @@ class RequestModal extends Component {
           <div className="modal-text">
             <div className="form">
               <h1 className="app-title">Request Shadowing</h1>
-              <h3 className="app-subtitle">{this.props.selectedDoctor.name}, {this.props.selectedDoctor.degree}</h3>
+              <h3 className="app-subtitle">{this.props.selectedDoctor.specialty} Physician, {this.props.selectedDoctor.degree}</h3>
               <div className="data-item">
                 <h5 className="app-subtitle">Name:</h5>
                 {this.props.student.name}
